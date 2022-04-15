789382e2d6e7ed837b2d7617a6cfb92470d6684c
Timothy Kinlaw <tkkinlaw@gmail.com>
2022-04-15 11:18:32 -0400

    IF Elif Else

---
+++ Python Syntax/If_Elif_Else.py
@@ -0,0 +1,11 @@
+a = 5 #switch out this number to see how the logic behaves
+
+if a > 5:
+    print("It is greater than five!!!")
+elif a == 5:
+    print("It is equal to five!!!")
+elif a < 5:
+    print("It is less than five")
+else:
+    print("No idea what is happening. Is it an integer?")
+print("All done")
