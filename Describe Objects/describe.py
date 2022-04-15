6b3c16020c1fbd31e7e4298500ee2833eff0f59f
Timothy Kinlaw <tkkinlaw@gmail.com>
2022-04-15 11:13:39 -0400

    arcpy.Describe

---
+++ Describe Objects/describe.py
@@ -0,0 +1,14 @@
+import arcpy
+
+fc = r"C:\Demos\PYTS\SanDiego.gdb\MajorAttractions"
+
+desc = arcpy.Describe(fc)
+
+print(desc)
+
+print(desc.shapeType)
+
+if desc.shapeType == "Point" and desc.datasetType == "FeatureClass":
+	arcpy.CreateFeatureclass_management(out_path=desc.path,
+                                   out_name="DownTown{}".format(desc.name), geometry_type="POLYGON", template=fc,
+                                    spatial_reference=desc.spatialReference)
