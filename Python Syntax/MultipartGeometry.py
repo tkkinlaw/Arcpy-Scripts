827d084ff8f4125090714306cd08ea6074e90f2c
Timothy Kinlaw <tkkinlaw@gmail.com>
2022-05-09 13:38:56 -0400

    Demo script

---
+++ Python Syntax/MultipartGeometry.py
@@ -0,0 +1,16 @@
+import arcpy
+
+fc = r"C:\Users\tim10393\OneDrive - Esri\DemoData\DemoData.gdb\WorldTerritories"
+fields = ["NAME", "Shape@"]
+whereClause = """NAME = 'United Kingdom'"""
+
+src = arcpy.da.SearchCursor(fc, fields, where_clause = whereClause)
+for row in src:
+    print(row)
+
+# The getPart method looks inside the polygon geometry object and grabs the first array of coordinates, being the
+# geometry of one of the many territories of the United Kingdom
+# By using this line of code outside the for loop, the row variable is set to the last iteration of the search cursor
+# That is a good strategy for just testing code :)
+arcpy.Polygon(row[1].getPart(0))
+
